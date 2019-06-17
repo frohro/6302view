29cefed04b5e38e04f8c605ff7854fda4738bd83
almonds0166 <almonds@mit.edu>
2019-06-17 01:09:05 -0400

    Update Six302.h
    * Limited number of controls and length of titles in order to compile on the Arduino Uno (currently compiles, but doesn't work).
    * Added state machine definitions
    * Prepared for ESPs and Uno

--- src/Six302.h
+++ src/Six302.h
@@ -1,8 +1,7 @@
 #ifndef _Six302_H_
 #define _Six302_H_
 
-#include "Arduino.h"
-
+#include <Arduino.h>
 #include <stdlib.h>
 #include <string.h>
 
@@ -10,11 +9,11 @@
 
 // How many elements you can add
 // Joystick counts as two controls
-#define MAX_CONTROLS      20
+#define MAX_CONTROLS      10
 #define MAX_REPORTERS     10
 
 // Max length of titles of controls & reporters
-#define MAX_TITLE_LENGTH  32
+#define MAX_TITLE_LENGTH  10
 
 // Incoming and outgoing message length limits
 #define MAX_IN_LEN 28 /* (id)(:)(float)(\n) = 2+1+24+1 = 28 */
@@ -28,68 +27,62 @@
 #define S302_SERIAL       0
 #define S302_WEBSOCKETS   1
 
-// Use macros to determine what board we're working with
-
-#define THIS_BOARD S302_ARDUINO_UNO // default assumption
-#ifdef ESP8266
-   #undef THIS_BOARD
-   #define THIS_BOARD S302_ESP8266
-#endif
-#ifdef ESP32
-   #undef THIS_BOARD
-   #define THIS_BOARD S302_ESP32
-#endif
-#ifdef TEENSYDUINO
-   #undef THIS_BOARD
-   #define THIS_BOARD S302_TEENSY
-#endif
-
 // States
 #define IDLE 0
+#define S302_DISCONNECTED 0
+#define S302_LISTEN       1
+#define S302_TALK         2
 
 class CommManager {
    public:
       // Main things
       CommManager(uint32_t sp=1000, uint32_t rp=20000);
-      bool connect(usb_serial_class* s, uint32_t baud); // Serial
-      bool connect(char* ssid, char* pw); // Pure wifi
-      bool step();
+
+      /* Serial */
+      #if defined TEENSYDUINO
+         void connect(usb_serial_class* s, uint32_t baud);
+      #else
+         void connect(HardwareSerial* s, uint32_t baud);
+      #endif
+      /* Pure wifi */
+      void connect(char* ssid, char* pw);
+      void step();
+      uint32_t headroom();
 
       // Add controls:
       bool addToggle(
          bool* linker,
-         char* title);
+         const char* title);
       bool addButton(
          bool* linker,
-         char* title);
+         const char* title);
       bool addSlider(
          float* linker,
-         char* title,
-         std::initializer_list<float> range,
+         const char* title,
+         float range_low, float range_high,
          float resolution,
          bool toggle=false);
       bool addJoystick(
          float* linker_x,
          float* linker_y,
-         char* title,
-         std::initializer_list<float> xrange,
-         std::initializer_list<float> yrange,
+         const char* title,
+         float xrange_low, float xrange_high,
+         float yrange_low, float yrange_high,
          float resolution,
          bool sticky=true);
 
       // Add reporters:
       bool addPlot(
          float* linker,
-         char* title,
-         std::initializer_list<float> yrange,
+         const char* title,
+         float yrange_low, float yrange_high,
          int steps_displayed=10,
          int num_plots=1);
-      bool addNumber(int32_t* linker, char* title);
-      bool addNumber(float* linker, char* title);
+      bool addNumber(int32_t* linker, const char* title);
+      bool addNumber(float* linker, const char* title);
 
       // Send debugging info
-      void debug(const char*);
-      //void debug(char*);
+      void debug(char*);
       //void debug(String);
       //void debug(bool);
       //void debug(int);
@@ -97,10 +90,13 @@ class CommManager {
       //void debug(double);
 
    protected:
-      uint32_t _wait();
       void _control();
       void _report();
-      bool _build();
+      void _wait_for_connection();
+      bool _time_to_talk();
+      void _wait();
+
+      int32_t _headroom;
 
       uint8_t _architecture;
 
@@ -117,10 +113,14 @@ class CommManager {
       char _incoming[MAX_IN_LEN];
       char _outgoing[MAX_OUT_LEN];
 
-      bool _connected;
+      uint8_t _state;
 
       // Serial
-      usb_serial_class* _serial;
+      #if defined TEENSYDUINO
+         usb_serial_class* _serial;
+      #else
+         HardwareSerial* _serial;
+      #endif
       uint32_t _baud;
       
       // Websockets
@@ -128,14 +128,14 @@ class CommManager {
 
       // Variables for timing
       #if defined TEENSYDUINO
-         elapsedMicros _timer;
-      #elif defined ESP32
-         uint32_t _timer;
-      #elif defined ESP8266
+         elapsedMicros _main_timer;
+         elapsedMicros _report_timer;
+      #elif defined (ESP32) || (ESP8266)
+         uint64_t _main_timer;
+         uint64_t _report_timer;
       #else // I'm assuming it's an Arduino Uno
       #endif
-
-      uint32_t _headroom;
+      
 };
 
 #endif
