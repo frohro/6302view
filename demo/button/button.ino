4a588cb5a2b0da2a13e608dbe8f66d8f65013823
almonds0166 <almonds@mit.edu>
2019-08-16 09:24:50 -0400

    Actually added the demos this time
    I forgot oops

---
+++ demo/button/button.ino
@@ -0,0 +1,35 @@
+
+#include "Six302.h"
+
+/* For this demo, make sure to have the `#define S302_SERIAL`
+   macro enabled in the library. 
+
+   This demo presents a button that when pressed increments
+   the number displayed */
+
+// microseconds
+#define STEP_TIME 100000
+#define REPORT_TIME 500000
+
+CommManager cm(STEP_TIME, REPORT_TIME);
+
+bool input;
+int32_t output;
+
+void setup() {
+   Serial.begin(115200);
+   while(!Serial);
+   
+   /* Add modules */
+   cm.addButton(&input, "Increment");
+   cm.addNumber(&output, "Sum");
+
+   /* Ready to communicate over Serial */
+   cm.connect(&Serial, 115200);
+}
+
+void loop() {
+   if( input )
+      output++;
+   cm.step();
+}
