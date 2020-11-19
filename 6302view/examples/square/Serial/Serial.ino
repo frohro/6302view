f6fb556d5e64a92889bed16de896064187bac3b0
Madison Landry <almonds@mit.edu>
2020-11-19 11:02:02 -0500

    Square example clock rate increase

--- 6302view/examples/square/Serial/Serial.ino
+++ 6302view/examples/square/Serial/Serial.ino
@@ -6,8 +6,8 @@
 #include <Six302.h>
 
 // microseconds
-#define STEP_TIME 100000
-#define REPORT_TIME 500000
+#define STEP_TIME 10000
+#define REPORT_TIME 50000
 
 CommManager cm(STEP_TIME, REPORT_TIME);
 
