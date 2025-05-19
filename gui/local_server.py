35622bca6d34ac21048675174df0dd5065d3cde7
frohro <rob.frohne@wallawalla.edu>
2025-05-19 22:13:49 +0000

    Add local server GUI functionality

--- gui/local_server.py
+++ gui/local_server.py
@@ -53,7 +53,7 @@ class MCU:
 VALID_MCUs: typing.List[MCU] = [
     MCU({5824}, "Teensy 3.2", "Teensy", "a Teensy"),
     MCU({10755, 9025}, "Arduino Uno (FTDI chipset)", "Arduino Uno", "an Uno"),
-    MCU({6790}, "ESP8266 D1 Mini Pro (with CH340 Adapter)", "ESP8266", "an ESP8266"),
+    MCU({6790}, "ESP8266 D1 Mini Pro (with CH340 Adapter) (or ESP32-S3)", "ESP8266", "an ESP8266"),
     MCU({4292, 12346}, "ESP32 Dev Module", "ESP32", "an ESP32"),
     MCU({11914}, "Raspberry Pi Pico", "Raspberry Pi Pico", "a Pico"),
 ]
